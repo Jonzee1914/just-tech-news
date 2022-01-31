a09bdf3a3d3f51ddf5a2098e118e1cc9c50b63a5
Erik Jones <jonzee1914@gmail.com>
2022-01-31 10:21:21 -0500

    updated from snapshot

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,8 +3,8 @@ const { User, Post } = require('../models');
 
 const userdata = [
   {
-    username: 'user1',
-    email: 'email@email.com',
+    username: 'alesmonde0',
+    email: 'nwestnedge0@cbc.ca',
     password: 'password123'
   },
   {
